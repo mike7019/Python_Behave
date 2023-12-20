6e9748014fd1b37d1cc470daa267b9497f2f7307
mike <amph7019@gmail.com>
2023-12-20 08:50:34 -0500

    interactions

--- features/pages/DashboardPage.py
+++ features/pages/DashboardPage.py
@@ -1,5 +1,4 @@
-import hamcrest
-from behave.model import Text
+import logging
 from selenium.webdriver.common.by import By
 
 
@@ -11,6 +10,8 @@ class AssertionUtils:
     txt_title = "//h1"
 
     def assert_dashboard_title(self, expected_title):
-        actual_title = self.driver.find_element(By.XPATH, self.txt_title).get_text()
+        actual_title = self.driver.find_element(By.XPATH, self.txt_title).text()
+        logging.info('Watch out!')  # will print a message to the console
+        print(actual_title)
         assert expected_title == actual_title
 
